9c5da01b674d1bf2659272289378dc33c600676f
anand-shukla-au5 <shuklaanand800@gmail.com>
2020-08-12 03:02:35 -0700

    Delicon

--- Delicon/backend/route/reservations.js
+++ Delicon/backend/route/reservations.js
@@ -4,14 +4,11 @@ const reservation = require('../model/reserve')
 const userModewl = require('../model/user')
 const router = express.Router()
 router.post('/read', userValid, async (req, res) => {
-    console.log(req.body)
-    let found = await userModewl.findOne({ email: req.body.email })
-    try {
-        let result = found.populate('reserves')
+    console.log(req.body.email)
+    userModewl.findOne({ email: req.body.email }).populate('reservation').exec(function (err, result) {
+        if (err) return console.log(err)
         res.json(result)
-    } catch (err) {
-        console.log(err)
-    }
+    })
 })
 router.post('/create', userValid, async (req, res) => {
     console.log(req.body)
@@ -28,17 +25,36 @@ router.post('/create', userValid, async (req, res) => {
     }
 })
 router.put('/edit', userValid, async (req, res) => {
-    let found = await userModewl.findOne({ email: req.body.email })
+    console.log(req.body)
+    const { name, food, people, floor, edit } = req.body
+    let found = await reservation.findByIdAndUpdate({ _id: edit }, { name, food, people, floor })
     try {
-        let reserve = await reservation.findOneAndUpdate()
-        found.reservation.push(reserve)
-        found.save()
+        console.log("f", found)
         res.json(found)
     } catch (err) {
         console.log(err)
     }
 })
 router.delete('/delete', userValid, (req, res) => {
-
+    reservation.findByIdAndDelete({ _id: req.body.id }, function (err, result) {
+        if (err) return console.log(err)
+        console.log(result)
+    })
+    userModewl.findOneAndUpdate(
+        { email: req.body.email },
+        {
+            $pull: { reservation: { _id: req.body.id } },
+        },
+        {
+            new: true,
+        },
+        (err, result) => {
+            if (err) {
+                return res.json(err)
+            }
+            console.log("deleted", result)
+            res.json(result)
+        }
+    );
 })
 module.exports = router
--- Delicon/client/src/Components/book.js
+++ Delicon/client/src/Components/book.js
@@ -12,14 +12,14 @@ const Book = () => {
     const [email, setEmail] = useState("")
     const [token, setToken] = useState("")
     const [arr, setArray] = useState([])
+    const [edit, setEdit] = useState("")
     useEffect(() => {
         if (!localStorage.getItem("auth-token")) {
             history.push('/')
         }
         else {
-            setEmail(localStorage.getItem("user"))
-            setToken(localStorage.getItem("auth-token"))
             let auth = localStorage.getItem("auth-token")
+            let email = localStorage.getItem("user")
             console.log("ASDAS", auth)
             axios({
                 method: 'POST',
@@ -28,13 +28,16 @@ const Book = () => {
                 headers: { "auth-token": auth }
             }).then(res => {
                 console.log(res)
+                setEmail(localStorage.getItem("user"))
+                setToken(localStorage.getItem("auth-token"))
+                setArray(res.data.reservation)
             }).catch(err => {
                 console.log(err)
             })
         }
-    }, [])
+    }, [history])
 
-    console.log(name, floor, people, food)
+    console.log(name, floor, people, food, arr)
     const addResvation = (e) => {
         e.preventDefault()
         const create = async () => {
@@ -46,32 +49,76 @@ const Book = () => {
                     headers: { "auth-token": token }
                 })
                 console.log(res.data)
+                window.location.reload()
             } catch (err) {
                 console.log(err)
             }
         }
         create()
     }
+
+    const editResvation = (e) => {
+        e.preventDefault()
+        const editData = async () => {
+            try {
+                let res = axios({
+                    method: 'PUT',
+                    url: 'http://localhost:8080/edit',
+                    data: { name, floor, people, food, edit },
+                    headers: { "auth-token": token }
+                })
+                console.log(res.data)
+                window.location.reload()
+            } catch (err) {
+                console.log(err)
+            }
+        }
+        editData()
+    }
+
+    const delReserve = (id) => {
+        console.log(id)
+        axios({
+            method: 'delete',
+            url: 'http://localhost:8080/delete',
+            data: { id: id },
+            headers: { "auth-token": token }
+        }).then(res => {
+            console.log(res)
+            window.location.reload()
+        }).catch(err => {
+            console.log(err)
+        })
+
+    }
+    const editReserve = (field) => {
+        console.log("Editting")
+        setEdit(field._id)
+        setName(field.name)
+        setPeople(field.people)
+        setFloor(field.floor)
+        setFood(field.food)
+    }
     return (
-        <div className="container-fluid">
+        <div className="container-fluid mb-5">
             <h1 className="text-info">Restaurant Reservation</h1>
             <div className="row">
-                <div class="col-md-5 offset-md-0">
+                <div class="col-md-5">
                     <form>
                         <div className="form-group">
                             <label for="exampleFormControlInput1">Restaurant Name</label>
-                            <input onChange={(e) => setName(e.target.value)} type="text" class="form-control" placeholder="Restaurant name" />
+                            <input value={name} onChange={(e) => setName(e.target.value)} type="text" class="form-control" placeholder="Restaurant name" />
                         </div>
                         <div className="form-group">
                             <label for="exampleFormControlSelect1">Floor</label>
-                            <select class="form-control" onChange={(e) => setFloor(e.target.value)} >
+                            <select value={floor} class="form-control" onChange={(e) => setFloor(e.target.value)} >
                                 <option>Ground Floor</option>
                                 <option>Roof Top</option>
                             </select>
                         </div>
                         <div className="form-group">
                             <label for="exampleFormControlSelect1">No of People</label>
-                            <select className="form-control" onChange={(e) => setPeople(e.target.value)} >
+                            <select value={people} className="form-control" onChange={(e) => setPeople(e.target.value)} >
                                 <option>1</option>
                                 <option>2</option>
                                 <option>3</option>
@@ -82,10 +129,29 @@ const Book = () => {
                         </div>
                         <div class="form-group">
                             <label for="exampleFormControlTextarea1">Food to be Served</label>
-                            <textarea className="form-control" onChange={(e) => setFood(e.target.value)} rows="2"></textarea>
+                            <textarea value={food} className="form-control" onChange={(e) => setFood(e.target.value)} rows="2"></textarea>
                         </div>
-                        <button type="submit" onClick={(e) => addResvation(e)} class="btn btn-primary">Submit</button>
+                        {edit === "" ? <button type="submit" onClick={(e) => addResvation(e)} class="btn btn-primary">Submit</button> : <button type="submit" onClick={(e) => editResvation(e)} class="btn btn-primary">Edit</button>}
                     </form>
+                </div>
+                <div className="col-md-6" >
+                    {arr && arr.map(el => {
+                        return (
+                            <div key={el._id} class="media shadow p-3 mb-5 bg-white rounded m-2">
+                                <img width="200px" src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSHxpU2ZbovkLf37SqbtpNg1vlk_C_8hBf8YA&usqp=" class="mr-3" alt="..." />
+                                <div class="media-body">
+                                    <h5 class="mt-0">Name: {el.name}</h5>
+                                    <h6>Food Itme: {el.food}</h6>
+                                    <p>No of People : {el.people}</p>
+                                </div>
+                                <div>
+                                    <button onClick={() => editReserve(el)} type="button" class="btn btn-info">Edit</button>
+                                    <button onClick={() => { delReserve(el._id) }} type="button" class="btn btn-danger ml-2">Delete</button>
+                                    <p className="mt-4">{el.floor}</p>
+                                </div>
+                            </div>
+                        )
+                    })}
                 </div>
             </div>
         </div>
