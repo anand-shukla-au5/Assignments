a6a9a9bfb98772c9e320b2a090a81aed76bf9395
anand-shukla-au5 <shuklaanand800@gmail.com>
2020-08-31 06:16:57 +0530

    booked

--- lineysha/src/Components/Userbook.js
+++ lineysha/src/Components/Userbook.js
@@ -1,9 +1,20 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
+import { Button } from "@material-ui/core";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
 
 export default function Userbook() {
   const [userdata, setData] = useState();
+  const [time, setTime] = useState(null);
+  const [date, setDate] = useState(null);
+  const [booked, setBooked] = useState([]);
   let { id } = useParams();
   useEffect(() => {
     const userFetch = async () => {
@@ -13,7 +24,18 @@ export default function Userbook() {
     };
     userFetch(id);
   }, [id]);
-  console.log(userdata);
+  console.log(userdata, time, date, "Booked", booked);
+  const handleBook = () => {
+    if (time && date) {
+      let arr = booked;
+      let obj = { time, date };
+      arr.push(obj);
+      setBooked(arr);
+      alert("Booked Succesfully");
+    } else {
+      alert("Please Choose date and time");
+    }
+  };
   return (
     <>
       {userdata && (
@@ -22,8 +44,41 @@ export default function Userbook() {
             {userdata.first_name} {userdata.last_name}
           </h1>
           <div className="profileData">
-            <img src={userdata.avatar} alt="" />
-            <p>{userdata.email}</p>
+            <div>
+              <img src={userdata.avatar} alt="" />
+              <p>{userdata.email}</p>
+              <Button
+                onClick={() => handleBook()}
+                id="buttonBook"
+                variant="contained"
+              >
+                Book
+              </Button>
+            </div>
+            <div className="booking">
+              <FormControl className="">
+                <InputLabel id="demo-simple-select-helper-label">
+                  Booking Time
+                </InputLabel>
+                <Select
+                  labelId="demo-simple-select-helper-label"
+                  id="demo-simple-select-helper"
+                  value={time}
+                  onChange={(e) => setTime(e.target.value)}
+                >
+                  <MenuItem value={15}>15 mins</MenuItem>
+                  <MenuItem value={30}>30 mins</MenuItem>
+                  <MenuItem value={45}>45 mins</MenuItem>
+                  <MenuItem value={60}>60 mins</MenuItem>
+                </Select>
+                <FormHelperText>Select the booking time</FormHelperText>
+              </FormControl>
+            </div>
+            <Calendar
+              className="calender"
+              onChange={(date) => setDate(date)}
+              value={date}
+            />
           </div>
         </div>
       )}
